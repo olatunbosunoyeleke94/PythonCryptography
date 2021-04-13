e721febb25d3f62ded33f25be09e2c39500580a6
olatunbosunoyeleke94 <olatunbosunoyeleke@rocketmail.com>
2021-04-13 15:32:58 +0100

    First Commit

---
+++ reverse_algorithm.py
@@ -0,0 +1,9 @@
+﻿message = 'This program explains reverse cipher.'
+
+translated = '' #Cipher Text is stored here
+i = len(message) - 1
+
+while i > 0:
+    translated = translated + message[i]
+    i = i - 1
+print("The Cipher text is :", translated)
